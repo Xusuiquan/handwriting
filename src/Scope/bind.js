28e788d3ec73d32c713d642b999a18f627cc2b0f
wxxusuiquan <wxxusuiquan@gf.com.cn>
2021-12-06 14:08:32 +0800

    fix: update bind method

--- src/Scope/bind.js
+++ src/Scope/bind.js
@@ -1,19 +1,6 @@
 /**
- * bind
- */
-Function.prototype.bind2 = function (context = window, ...args1) {
-    const fn = this
-    return function newFn(...args2) {
-        // 判断是否用于构造函数
-        if (this instanceof newFn) {
-            return new fn(...args1, ...args2)
-        }
-        return fn.apply(context, [...args1, ...args2])
-    }
-}
-
-/**
  * 实现 bind
+ * bind 方法返回一个函数，需要有新的参数结合原有的参数
  */
 Function.prototype.myBind = function (context, ...args) {
     return (...newArgs) => this.apply(context, [...args, ...newArgs])
